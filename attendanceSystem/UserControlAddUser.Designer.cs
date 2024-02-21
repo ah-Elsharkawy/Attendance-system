f4018eaefab3de5de56835b879ae0c688b609d14
abdullah-mohamed9 <121031158+abdullah-mohamed9@users.noreply.github.com>
2024-02-21 17:37:41 +0200

    add user form

---
+++ attendanceSystem/UserControlAddUser.Designer.cs
@@ -0,0 +1,460 @@
+﻿namespace attendanceSystem
+{
+    partial class UserControlAddUser
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            toolTip = new ToolTip(components);
+            tabControlAddUser = new TabControl();
+            tabPageAdd = new TabPage();
+            tabPage2 = new TabPage();
+            label1 = new Label();
+            label2 = new Label();
+            textBoxName = new TextBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            textBoxEmail = new TextBox();
+            label3 = new Label();
+            panel3 = new Panel();
+            textBoxPassword = new TextBox();
+            label4 = new Label();
+            panel4 = new Panel();
+            textBoxClass = new TextBox();
+            label5 = new Label();
+            buttonAdd = new Button();
+            tabPageUpdate = new TabPage();
+            buttonUpdate = new Button();
+            panel5 = new Panel();
+            textBoxClassU = new TextBox();
+            label6 = new Label();
+            panel6 = new Panel();
+            textBoxPasswordU = new TextBox();
+            label7 = new Label();
+            panel7 = new Panel();
+            textBoxEmailU = new TextBox();
+            label8 = new Label();
+            panel8 = new Panel();
+            textBoxNameU = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            tabControlAddUser.SuspendLayout();
+            tabPageAdd.SuspendLayout();
+            tabPageUpdate.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControlAddUser
+            // 
+            tabControlAddUser.Alignment = TabAlignment.Bottom;
+            tabControlAddUser.Anchor = AnchorStyles.None;
+            tabControlAddUser.Controls.Add(tabPage2);
+            tabControlAddUser.Controls.Add(tabPageAdd);
+            tabControlAddUser.Controls.Add(tabPageUpdate);
+            tabControlAddUser.Font = new Font("Century Gothic", 9.75F);
+            tabControlAddUser.Location = new Point(0, 0);
+            tabControlAddUser.Name = "tabControlAddUser";
+            tabControlAddUser.SelectedIndex = 0;
+            tabControlAddUser.Size = new Size(942, 501);
+            tabControlAddUser.TabIndex = 0;
+            // 
+            // tabPageAdd
+            // 
+            tabPageAdd.Controls.Add(buttonAdd);
+            tabPageAdd.Controls.Add(panel4);
+            tabPageAdd.Controls.Add(textBoxClass);
+            tabPageAdd.Controls.Add(label5);
+            tabPageAdd.Controls.Add(panel3);
+            tabPageAdd.Controls.Add(textBoxPassword);
+            tabPageAdd.Controls.Add(label4);
+            tabPageAdd.Controls.Add(panel2);
+            tabPageAdd.Controls.Add(textBoxEmail);
+            tabPageAdd.Controls.Add(label3);
+            tabPageAdd.Controls.Add(panel1);
+            tabPageAdd.Controls.Add(textBoxName);
+            tabPageAdd.Controls.Add(label2);
+            tabPageAdd.Controls.Add(label1);
+            tabPageAdd.Location = new Point(4, 4);
+            tabPageAdd.Name = "tabPageAdd";
+            tabPageAdd.Padding = new Padding(3);
+            tabPageAdd.Size = new Size(934, 467);
+            tabPageAdd.TabIndex = 0;
+            tabPageAdd.Text = "Add User";
+            tabPageAdd.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 4);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(934, 467);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(67, 31, 125);
+            label1.Location = new Point(6, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Add User";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold);
+            label2.Location = new Point(112, 122);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Name:";
+            // 
+            // textBoxName
+            // 
+            textBoxName.BorderStyle = BorderStyle.None;
+            textBoxName.Font = new Font("Century Gothic", 10F);
+            textBoxName.Location = new Point(115, 151);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(270, 21);
+            textBoxName.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.LightGray;
+            panel1.Location = new Point(115, 175);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(270, 2);
+            panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.LightGray;
+            panel2.Location = new Point(553, 180);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(270, 2);
+            panel2.TabIndex = 6;
+            // 
+            // textBoxEmail
+            // 
+            textBoxEmail.BorderStyle = BorderStyle.None;
+            textBoxEmail.Font = new Font("Century Gothic", 10F);
+            textBoxEmail.Location = new Point(553, 156);
+            textBoxEmail.Name = "textBoxEmail";
+            textBoxEmail.Size = new Size(270, 21);
+            textBoxEmail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold);
+            label3.Location = new Point(553, 122);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 19);
+            label3.TabIndex = 4;
+            label3.Text = "Email:";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.LightGray;
+            panel3.Location = new Point(112, 246);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(270, 2);
+            panel3.TabIndex = 9;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.BorderStyle = BorderStyle.None;
+            textBoxPassword.Font = new Font("Century Gothic", 10F);
+            textBoxPassword.Location = new Point(112, 222);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(270, 21);
+            textBoxPassword.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold);
+            label4.Location = new Point(112, 190);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 19);
+            label4.TabIndex = 7;
+            label4.Text = "password";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.LightGray;
+            panel4.Location = new Point(553, 246);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(270, 2);
+            panel4.TabIndex = 12;
+            // 
+            // textBoxClass
+            // 
+            textBoxClass.BorderStyle = BorderStyle.None;
+            textBoxClass.Font = new Font("Century Gothic", 10F);
+            textBoxClass.Location = new Point(553, 222);
+            textBoxClass.Name = "textBoxClass";
+            textBoxClass.Size = new Size(270, 21);
+            textBoxClass.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold);
+            label5.Location = new Point(553, 190);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 19);
+            label5.TabIndex = 10;
+            label5.Text = "class";
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.BackColor = Color.FromArgb(76, 3, 125);
+            buttonAdd.FlatAppearance.BorderSize = 0;
+            buttonAdd.FlatStyle = FlatStyle.Flat;
+            buttonAdd.ForeColor = Color.White;
+            buttonAdd.Location = new Point(112, 291);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(114, 37);
+            buttonAdd.TabIndex = 13;
+            buttonAdd.Text = "Add";
+            buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // tabPageUpdate
+            // 
+            tabPageUpdate.Controls.Add(buttonUpdate);
+            tabPageUpdate.Controls.Add(panel5);
+            tabPageUpdate.Controls.Add(textBoxClassU);
+            tabPageUpdate.Controls.Add(label6);
+            tabPageUpdate.Controls.Add(panel6);
+            tabPageUpdate.Controls.Add(textBoxPasswordU);
+            tabPageUpdate.Controls.Add(label7);
+            tabPageUpdate.Controls.Add(panel7);
+            tabPageUpdate.Controls.Add(textBoxEmailU);
+            tabPageUpdate.Controls.Add(label8);
+            tabPageUpdate.Controls.Add(panel8);
+            tabPageUpdate.Controls.Add(textBoxNameU);
+            tabPageUpdate.Controls.Add(label9);
+            tabPageUpdate.Controls.Add(label10);
+            tabPageUpdate.Location = new Point(4, 4);
+            tabPageUpdate.Name = "tabPageUpdate";
+            tabPageUpdate.Padding = new Padding(3);
+            tabPageUpdate.Size = new Size(934, 467);
+            tabPageUpdate.TabIndex = 2;
+            tabPageUpdate.Text = "Update User";
+            tabPageUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            buttonUpdate.BackColor = Color.FromArgb(76, 3, 125);
+            buttonUpdate.FlatAppearance.BorderSize = 0;
+            buttonUpdate.FlatStyle = FlatStyle.Flat;
+            buttonUpdate.ForeColor = Color.White;
+            buttonUpdate.Location = new Point(112, 291);
+            buttonUpdate.Name = "buttonUpdate";
+            buttonUpdate.Size = new Size(114, 37);
+            buttonUpdate.TabIndex = 27;
+            buttonUpdate.Text = "Update";
+            buttonUpdate.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.LightGray;
+            panel5.Location = new Point(553, 246);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(270, 2);
+            panel5.TabIndex = 26;
+            // 
+            // textBoxClassU
+            // 
+            textBoxClassU.BorderStyle = BorderStyle.None;
+            textBoxClassU.Font = new Font("Century Gothic", 10F);
+            textBoxClassU.Location = new Point(553, 222);
+            textBoxClassU.Name = "textBoxClassU";
+            textBoxClassU.Size = new Size(270, 21);
+            textBoxClassU.TabIndex = 25;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold);
+            label6.Location = new Point(553, 190);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 19);
+            label6.TabIndex = 24;
+            label6.Text = "class";
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.LightGray;
+            panel6.Location = new Point(112, 246);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(270, 2);
+            panel6.TabIndex = 23;
+            // 
+            // textBoxPasswordU
+            // 
+            textBoxPasswordU.BorderStyle = BorderStyle.None;
+            textBoxPasswordU.Font = new Font("Century Gothic", 10F);
+            textBoxPasswordU.Location = new Point(112, 222);
+            textBoxPasswordU.Name = "textBoxPasswordU";
+            textBoxPasswordU.Size = new Size(270, 21);
+            textBoxPasswordU.TabIndex = 22;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold);
+            label7.Location = new Point(112, 190);
+            label7.Name = "label7";
+            label7.Size = new Size(88, 19);
+            label7.TabIndex = 21;
+            label7.Text = "password";
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.LightGray;
+            panel7.Location = new Point(553, 180);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(270, 2);
+            panel7.TabIndex = 20;
+            // 
+            // textBoxEmailU
+            // 
+            textBoxEmailU.BorderStyle = BorderStyle.None;
+            textBoxEmailU.Font = new Font("Century Gothic", 10F);
+            textBoxEmailU.Location = new Point(553, 156);
+            textBoxEmailU.Name = "textBoxEmailU";
+            textBoxEmailU.Size = new Size(270, 21);
+            textBoxEmailU.TabIndex = 19;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold);
+            label8.Location = new Point(553, 122);
+            label8.Name = "label8";
+            label8.Size = new Size(58, 19);
+            label8.TabIndex = 18;
+            label8.Text = "Email:";
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.LightGray;
+            panel8.Location = new Point(115, 175);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(270, 2);
+            panel8.TabIndex = 17;
+            // 
+            // textBoxNameU
+            // 
+            textBoxNameU.BorderStyle = BorderStyle.None;
+            textBoxNameU.Font = new Font("Century Gothic", 10F);
+            textBoxNameU.Location = new Point(115, 151);
+            textBoxNameU.Name = "textBoxNameU";
+            textBoxNameU.Size = new Size(270, 21);
+            textBoxNameU.TabIndex = 16;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold);
+            label9.Location = new Point(112, 122);
+            label9.Name = "label9";
+            label9.Size = new Size(65, 19);
+            label9.TabIndex = 15;
+            label9.Text = "Name:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            label10.ForeColor = Color.FromArgb(67, 31, 125);
+            label10.Location = new Point(6, 3);
+            label10.Name = "label10";
+            label10.Size = new Size(128, 23);
+            label10.TabIndex = 14;
+            label10.Text = "Update User";
+            // 
+            // UserControlAddUser
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(tabControlAddUser);
+            Font = new Font("Century Gothic", 9.75F);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "UserControlAddUser";
+            Size = new Size(942, 501);
+            tabControlAddUser.ResumeLayout(false);
+            tabPageAdd.ResumeLayout(false);
+            tabPageAdd.PerformLayout();
+            tabPageUpdate.ResumeLayout(false);
+            tabPageUpdate.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ToolTip toolTip;
+        private TabControl tabControlAddUser;
+        private TabPage tabPageAdd;
+        private TabPage tabPage2;
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxName;
+        private Panel panel2;
+        private TextBox textBoxEmail;
+        private Label label3;
+        private Panel panel1;
+        private Panel panel3;
+        private TextBox textBoxPassword;
+        private Label label4;
+        private Panel panel4;
+        private TextBox textBoxClass;
+        private Label label5;
+        private Button buttonAdd;
+        private TabPage tabPageUpdate;
+        private Button buttonUpdate;
+        private Panel panel5;
+        private TextBox textBoxClassU;
+        private Label label6;
+        private Panel panel6;
+        private TextBox textBoxPasswordU;
+        private Label label7;
+        private Panel panel7;
+        private TextBox textBoxEmailU;
+        private Label label8;
+        private Panel panel8;
+        private TextBox textBoxNameU;
+        private Label label9;
+        private Label label10;
+    }
+}
