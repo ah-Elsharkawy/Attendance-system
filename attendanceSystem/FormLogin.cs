a895cd9243c197d96fe4b0d5d7a8a325465b37cc
Fatma Atef <fatma.atef.abdelhady@gmail.com>
2024-02-22 23:56:13 +0200

    after adding loginDesign

---
+++ attendanceSystem/FormLogin.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance_Management_System.PAL.Forms
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void pictureBoxMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxShow_MouseHover(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(pictureBoxShow, "show Password");
+        }
+
+        private void pictureBoxHide_MouseHover(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(pictureBoxHide, "Hide Password");
+        }
+
+        private void pictureBoxShow_Click(object sender, EventArgs e)
+        {
+            textBoxPassword.UseSystemPasswordChar = false;
+            pictureBoxShow.Hide();
+            pictureBoxHide.Show();
+        }
+
+        private void pictureBoxHide_Click(object sender, EventArgs e)
+        {
+            textBoxPassword.UseSystemPasswordChar = true;
+            pictureBoxShow.Show();
+            pictureBoxHide.Hide();
+        }
+
+        private void pictureBoxError_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            pictureBoxHide.Hide();
+            pictureBoxError.Hide();
+            labelError.Hide();
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxClose_MouseHover(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(pictureBoxClose, "Close");
+
+        }
+
+        private void pictureBoxMinimize_MouseHover(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(pictureBoxMinimize, "Minimize");
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string check = Attendance.Attendance.IsValidNamePass(textBoxName.Text.Trim(),textBoxPassword.Text.Trim, XmlReadMode)
+        }
+    }
+}
